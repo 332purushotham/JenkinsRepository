f12debd222ef2edb174227ce067bc17b31b1b826
332purushotham <purushothaminfo71@gmail.com>
2023-09-06 22:51:49 +0530

    this is for jenkins

---
+++ OneNewApplication/src/main/java/LunchBrowser.java
@@ -0,0 +1,12 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LunchBrowser {
+    public static void main(String[] args) {
+    	WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.selenium.dev/selenium/web/web-form.html");
+    }
+}
